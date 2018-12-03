15e43dcc2fa03db38c0c33623758651f4504504f
Aaron Macy <aaronmacy@gmail.com>
2018-12-03 12:45:54 -0800

    day 1, part 2

--- lib/day_01.rb
+++ lib/day_01.rb
@@ -1,7 +1,24 @@
+require "set"
+
 module Day01
   INPUT = File.readlines("config/day_01.txt").map { |line| line.chomp.to_i }
 
-  def self.process(input=INPUT)
+  def self.resulting_frequency(input=INPUT)
     input.inject(&:+)
+  end
+
+  def self.first_frequency_reached_twice(input=INPUT)
+    current_frequency = 0
+    reached = Set.new
+    reached << current_frequency
+
+    loop do
+      input.each do |i|
+        new_frequency = current_frequency + i
+        return new_frequency if reached.include?(new_frequency)
+        reached << new_frequency
+        current_frequency = new_frequency
+      end
+    end
   end
 end
--- spec/day_01_spec.rb
+++ spec/day_01_spec.rb
@@ -2,22 +2,44 @@ require_relative "../lib/day_01.rb"
 require "pry"
 
 RSpec.describe Day01 do
-  it "calculates the resulting frequency using sample input resulting in a positive total" do
-    input = [1, 1, 1]
-    expect(Day01.process(input)).to eq 3
+  context "part 1" do
+    it "calculates the resulting frequency using sample input resulting in a positive total" do
+      input = [1, 1, 1]
+      expect(Day01.resulting_frequency(input)).to eq 3
+    end
+
+    it "calculates the resulting frequency using sample input resulting in a negative total" do
+      input = [-1, -2, -3]
+      expect(Day01.resulting_frequency(input)).to eq -6
+    end
+
+    it "calculates the resulting frequency using sample input resulting in a 0 total" do
+      input = [1, 1, -2]
+      expect(Day01.resulting_frequency(input)).to eq 0
+    end
+
+    it "calculates the resulting frequency using input file" do
+      expect(Day01.resulting_frequency).to eq 406
+    end
   end
 
-  it "calculates the resulting frequency using sample input resulting in a negative total" do
-    input = [-1, -2, -3]
-    expect(Day01.process(input)).to eq -6
-  end
+  context "part 2" do
+    it "returns the first frequency received twice if it's 0" do
+      input_1 = [1, -1]
+      expect(Day01.first_frequency_reached_twice(input_1)).to eq 0
 
-  it "calculates the resulting frequency using sample input resulting in a 0 total" do
-    input = [1, 1, -2]
-    expect(Day01.process(input)).to eq 0
-  end
+      input_2 = [3, 3, 4, -2, -4]
+      expect(Day01.first_frequency_reached_twice(input_2)).to eq 10
 
-  it "calculates the resulting frequency using input file" do
-    expect(Day01.process).to eq 406
+      input_3 = [-6, 3, 8, 5, -6]
+      expect(Day01.first_frequency_reached_twice(input_3)).to eq 5
+
+      input_4 = [7, 7, -2, -7, -4]
+      expect(Day01.first_frequency_reached_twice(input_4)).to eq 14
+    end
+
+    it "calculates the first frequency using input file" do
+      expect(Day01.first_frequency_reached_twice).to eq 312
+    end
   end
 end
