224ec86e81da5dafd63829a3ad612ffb608cb6f3
Aaron Macy <aaronmacy@gmail.com>
2018-12-05 13:20:24 -0800

    day 3, part 2

--- lib/day_03.rb
+++ lib/day_03.rb
@@ -2,10 +2,47 @@ require "set"
 
 module Day03
   INPUT = File.readlines("config/day_03.txt").map(&:chomp)
-  Claim = Struct.new(:x, :y, :width, :height)
+  Claim = Struct.new(:id, :x, :y, :width, :height)
 
   def self.overlapping_area(input=INPUT)
     claims = _parse_input(input)
+    graph = _generate_graph(claims)
+
+    graph.inject(0) do |sum, row|
+      next sum if row.nil?
+      row.each do |node|
+        next sum if node.nil?
+        sum += 1 if node.length > 1
+      end
+      sum
+    end
+  end
+
+  def self.non_overlapping_claim(input=INPUT)
+    claims = _parse_input(input)
+    graph = _generate_graph(claims)
+
+    solo_nodes = []
+    graph.each_with_index do |row, x|
+      next if row.nil?
+      row.each_with_index do |node, y|
+        next if node.nil?
+        solo_nodes << node.first if node.length == 1
+      end
+    end
+
+    claim_to_count = solo_nodes.flatten.inject({}) do |claim_to_count, claim|
+      claim_to_count[claim] ||= 0
+      claim_to_count[claim] += 1
+      claim_to_count
+    end
+
+    claim_to_count.each do |claim, count|
+      return claim.id if claim.width * claim.height == count
+    end
+  end
+
+  def self._generate_graph(claims)
     graph = []
 
     claims.each do |claim|
@@ -18,24 +55,19 @@ module Day03
       end
     end
 
-    graph.inject(0) do |sum, row|
-      next sum if row.nil?
-      row.each do |node|
-        next sum if node.nil?
-        sum += 1 if node.length > 1
-      end
-      sum
-    end
+    graph
   end
 
   def self._parse_input(input)
     input.map do |line|
-      coordinates, dimensions = line.split(/@ /).last.split(": ")
+      raw_id, remainder = line.split(/ @ /)
+      coordinates, dimensions = remainder.split(": ")
 
+      id = raw_id.gsub("#", "").to_i
       x, y = coordinates.split(",").map(&:to_i)
       width, height = dimensions.split("x").map(&:to_i)
 
-      Claim.new(x, y, width, height)
+      Claim.new(id, x, y, width, height)
     end
   end
 end
--- spec/day_03_spec.rb
+++ spec/day_03_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Day03 do
       expect(Day03.overlapping_area).to eq 103806
     end
   end
+
+  context "part 2" do
+    it "finds the non-overlapping claims in the sample input" do
+      input = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "##3 @ 5,5: 2x2"]
+      expect(Day03.non_overlapping_claim(input)).to eq 3
+    end
+
+    it "finds the overlapping areas in the input file" do
+      expect(Day03.non_overlapping_claim).to eq 625
+    end
+  end
 end
