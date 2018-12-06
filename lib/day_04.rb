c5d1c9bb1693590e09b133dea25bd4c093703b47
Aaron Macy <aaronmacy@gmail.com>
2018-12-05 22:47:13 -0800

    day 4, part 1

---
+++ lib/day_04.rb
@@ -0,0 +1,103 @@
+require "date"
+
+module Day04
+  INPUT = File.readlines("config/day_04.txt").map(&:chomp)
+
+  class Entry
+    attr_reader :time, :text, :time_for_sorting
+
+    def initialize(line)
+      @raw_timestamp, @text = line.gsub("[", "").split("] ")
+      @time = _parse_time
+    end
+
+    def julian_date
+      # Time#to_date returns the wrong date for some times
+      normalized_time = _normalized_time
+      date = Date.new(normalized_time.year, normalized_time.month, normalized_time.day)
+      date.jd
+    end
+
+    def _normalized_time
+      if @time.hour == 23
+        hour = 0
+        minute = 0
+        if @time.day == 31
+          day = 1
+          month = @time.month + 1
+        else
+          day = @time.day + 1
+        end
+      end
+
+      Time.new(
+        @time.year,
+        month || @time.month,
+        day || @time.day,
+        hour || @time.hour,
+        minute || @time.min,
+      )
+    end
+
+    def _parse_time
+      day_remainder, hour_remainder = @raw_timestamp.split(" ")
+
+      year, month, day = day_remainder.split("-").map(&:to_i)
+      hour, minute = hour_remainder.split(":").map(&:to_i)
+
+      Time.new(year, month, day, hour, minute)
+    end
+  end
+
+  def self.strategy_1(input=INPUT)
+    graph = {}
+    guard_to_min_slept = {}
+    current_guard = nil
+    sleeping_start_time = nil
+    sleeping = false
+
+    entries = input.map do |line|
+      Entry.new(line)
+    end.sort { |a, b| a.time <=> b.time }
+
+    entries.each do |entry|
+      if entry.text.include?("Guard")
+        graph[entry.julian_date] ||= []
+
+        current_guard = entry.text.scan(/\d+/).first.to_i
+        guard_to_min_slept[current_guard] ||= 0
+      elsif entry.text.include?("falls asleep")
+        sleeping = true
+        sleeping_start_time = entry.time
+      elsif entry.text.include?("wakes up")
+        sleeping = false
+        (sleeping_start_time.min...entry.time.min).each do |minute|
+          guard_to_min_slept[current_guard] += 1
+          graph[entry.julian_date][minute] = current_guard
+        end
+        sleeping_start_time = nil
+      end
+
+      if sleeping
+        guard_to_min_slept[current_guard] += 1
+        graph[entry.julian_date][entry.time.min] = current_guard
+      end
+    end
+
+    sleepiest_guard = guard_to_min_slept.key(guard_to_min_slept.values.max)
+    sleep_by_minute = {}
+    graph.each_with_index do |(_, row), _|
+      next if row.nil?
+      row.each_with_index do |guard, minute|
+        if guard == sleepiest_guard
+          sleep_by_minute[minute] ||= 0
+          sleep_by_minute[minute] += 1
+        end
+      end
+    end
+
+    most_frequent_minute = sleep_by_minute.key(sleep_by_minute.values.max)
+
+    sleepiest_guard * most_frequent_minute
+  end
+end
---
+++ spec/day_04_spec.rb
@@ -0,0 +1,31 @@
+require_relative "../lib/day_04.rb"
+require "pry"
+
+RSpec.describe Day04 do
+  it "works for the sample input" do
+    input = [
+      "[1518-11-01 00:00] Guard #10 begins shift",
+      "[1518-11-01 00:05] falls asleep",
+      "[1518-11-01 00:25] wakes up",
+      "[1518-11-01 00:30] falls asleep",
+      "[1518-11-01 00:55] wakes up",
+      "[1518-11-01 23:58] Guard #99 begins shift",
+      "[1518-11-02 00:40] falls asleep",
+      "[1518-11-02 00:50] wakes up",
+      "[1518-11-03 00:05] Guard #10 begins shift",
+      "[1518-11-03 00:24] falls asleep",
+      "[1518-11-03 00:29] wakes up",
+      "[1518-11-04 00:02] Guard #99 begins shift",
+      "[1518-11-04 00:36] falls asleep",
+      "[1518-11-04 00:46] wakes up",
+      "[1518-11-05 00:03] Guard #99 begins shift",
+      "[1518-11-05 00:45] falls asleep",
+      "[1518-11-05 00:55] wakes up",
+    ]
+    expect(Day04.strategy_1(input)).to eq 240
+  end
+
+  it "works for the input file" do
+    expect(Day04.strategy_1).to eq 26281
+  end
+end
