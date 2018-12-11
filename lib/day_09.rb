71495186c79568118c9e94e926932a29148cf455
Aaron Macy <aaronmacy@gmail.com>
2018-12-10 22:37:03 -0800

    ~10 seconds of speed improvements but part one is still too slow (~16 sec)

--- lib/day_09.rb
+++ lib/day_09.rb
@@ -1,5 +1,5 @@
 class Day09
-  def self.part_1(player_count, highest_marble)
+  def self.solve(player_count, highest_marble)
     scores = (1..player_count).inject({}) do |hash, player|
       hash[player] = 0
       hash
@@ -19,20 +19,21 @@ class Day09
           negative_index = current_marble_index - 7
           current_marble_index = board.length + negative_index
         end
-        scores[current_player] += next_marble
-        scores[current_player] += board.delete_at(current_marble_index)
+        scores[current_player] += next_marble + board.delete_at(current_marble_index)
         current_marble = board[current_marble_index]
       else
         next_marble_index = _next_marble_index(board, current_marble_index)
 
         if board[next_marble_index]
-          board = board[0...next_marble_index] + [next_marble] + board[next_marble_index..board.length - 1]
+          part_1 = board[0...next_marble_index]
+          current_marble_index = part_1.length
+          board = part_1 + [next_marble] + board[next_marble_index..board.length - 1]
         else
-          board[next_marble_index] = next_marble
+          board << next_marble
+          current_marble_index = next_marble_index
         end
 
         current_marble = next_marble
-        current_marble_index = board.index(current_marble)
       end
 
       current_player += 1
--- lib/day_09_spec.rb
+++ lib/day_09_spec.rb
@@ -2,18 +2,21 @@ require_relative "../lib/day_09.rb"
 require "pry"
 
 RSpec.describe Day09 do
-  context "part 1" do
-    it "returns the correct answer for the sample input" do
-      expect(Day09.part_1(9, 25)).to eq 32
-      expect(Day09.part_1(10, 1618)).to eq 8317
-      expect(Day09.part_1(13, 7999)).to eq 146373
-      expect(Day09.part_1(17, 1104)).to eq 2764
-      expect(Day09.part_1(21, 6111)).to eq 54718
-      expect(Day09.part_1(30, 5807)).to eq 37305
-    end
+  it "returns the correct answer for the sample input" do
+    expect(Day09.solve(9, 25)).to eq 32
+    expect(Day09.solve(10, 1618)).to eq 8317
+    expect(Day09.solve(13, 7999)).to eq 146373
+    expect(Day09.solve(17, 1104)).to eq 2764
+    expect(Day09.solve(21, 6111)).to eq 54718
+    expect(Day09.solve(30, 5807)).to eq 37305
+  end
 
-    it "returns the correct answer for the sample file" do
-      expect(Day09.part_1(446, 71522)).to eq 390592
-    end
+  it "returns the correct answer for the sample file" do
+    expect(Day09.solve(446, 71522)).to eq 390592
+  end
+
+  it "returns the correct answer for part 2" do
+    skip "too slow"
+    expect(Day09.solve(446, 71522 * 100)).to eq 390592
   end
 end
