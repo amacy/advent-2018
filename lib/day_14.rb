448407bbcb25a4357197780d9b67c83c09d2f65f
Aaron Macy <aaronmacy@gmail.com>
2018-12-14 22:13:36 -0800

    likely works but too slow

--- lib/day_14.rb
+++ lib/day_14.rb
@@ -4,14 +4,33 @@ class Day14
   TRAILING_DIGITS = 10
 
   def self.part_1(practice_count)
+    _solve do |scores|
+      if scores.length >= practice_count + TRAILING_DIGITS
+        return scores[practice_count...practice_count + TRAILING_DIGITS].join
+      end
+    end
+  end
+
+  def self.part_2(score_sequence)
+    iterations = 0
+    score_sequence_length = score_sequence.length
+    _solve do |scores|
+      starting_index = scores.length - 2 - score_sequence_length
+      end_of_string = scores[starting_index..-1]
+      if end_of_string && end_of_string.join.match?(score_sequence)
+        match = scores.join.match(/^(.*?)#{score_sequence}/)
+        return match.captures.first.length if match
+      end
+    end
+  end
+
+  def self._solve(&block)
     elf_1_location = 0
     elf_2_location = 1
     scores = [ELF_1_STARTING_SCORE, ELF_2_STARTING_SCORE]
 
     loop do
-      if scores.length >= practice_count + TRAILING_DIGITS
-        return scores[practice_count...practice_count + TRAILING_DIGITS].join
-      end
+      yield(scores)
       new_scores = (scores[elf_1_location] + scores[elf_2_location]).to_s.split(//).map(&:to_i)
 
       new_scores.each { |i| scores << i }
@@ -25,13 +44,14 @@ class Day14
     new_current_location = nil
     loop do
       return new_current_location if new_current_location && scores[new_current_location]
-      if moves >= scores.length
-        moves = moves % scores.length
+      scores_length = scores.length
+      if moves >= scores_length
+        moves = moves % scores_length
       else
         if scores[current_location + moves]
           new_current_location = current_location + moves
         else
-          new_current_location = current_location + moves - scores.length
+          new_current_location = current_location + moves - scores_length
         end
       end
     end
--- spec/day_14_spec.rb
+++ spec/day_14_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe Day14 do
       expect(Day14.part_1(380621)).to eq "6985103122"
     end
   end
+
+  context "part 2" do
+    it "returns the correct answer for the sample input" do
+      expect(Day14.part_2("01245")).to eq 5
+      expect(Day14.part_2("51589")).to eq 9
+      expect(Day14.part_2("92510")).to eq 18
+      expect(Day14.part_2("59414")).to eq 2018
+    end
+
+    it "returns the correct answer for the puzzle input" do
+      expect(Day14.part_2("380621")).to eq 6985103122
+    end
+  end
 end
